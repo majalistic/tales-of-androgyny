05d8f09bf829f77cc62f331934078bbc5717633a
Majalis <majalistic@gmail.com>
2016-09-23 22:20:21 -0400

    Removing extraneous imports.

--- core/src/com/majalis/battle/BattleFactory.java
+++ core/src/com/majalis/battle/BattleFactory.java
@@ -3,7 +3,6 @@ package com.majalis.battle;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.math.Vector2;
 import com.majalis.character.EnemyCharacter;
 import com.majalis.character.PlayerCharacter;
 import com.majalis.save.LoadService;
--- core/src/com/majalis/character/EnemyCharacter.java
+++ core/src/com/majalis/character/EnemyCharacter.java
@@ -2,7 +2,6 @@ package com.majalis.character;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.IntArray;
 import com.majalis.battle.BattleFactory.EnemyEnum;
 
