1e6384960e068bcb49b60f021fea5d0d81c2c516
Majalis <majalistic@gmail.com>
2017-07-03 23:16:10 -0400

    Game type scene also has keyboard selection now.

--- core/src/com/majalis/scenes/GameTypeScene.java
+++ core/src/com/majalis/scenes/GameTypeScene.java
@@ -1,13 +1,17 @@
 package com.majalis.scenes;
 
-import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.OrderedMap;
-import com.majalis.asset.AssetEnum;
 import com.majalis.encounter.Background;
 import com.majalis.save.SaveService;
 import com.majalis.talesofandrogyny.TalesOfAndrogyny;
@@ -15,40 +19,76 @@ import com.majalis.talesofandrogyny.TalesOfAndrogyny;
  * Represents a choice displayed to the user in the course of an encounter.
  */
 public class GameTypeScene extends AbstractChoiceScene {
-
-	public static final Array<AssetDescriptor<?>> resourceRequirements = new Array<AssetDescriptor<?>>();
-	static {
-		resourceRequirements.add(AssetEnum.UI_SKIN.getSkin());
-		resourceRequirements.add(AssetEnum.BUTTON_SOUND.getSound());
-		resourceRequirements.add(AssetEnum.GAME_TYPE_BACKGROUND.getTexture());
-	}
-	// this should receive a map of integers to choice buttons 
+	private final Array<TextButton> buttons;
+	private int selection;
 	public GameTypeScene(OrderedMap<Integer, Scene> sceneBranches, int sceneCode, SaveService saveService, Array<TextButton> buttons, Background background) {
 		super(sceneBranches, sceneCode, saveService);
+		this.buttons = new Array<TextButton>();
 		this.addActor(background);
         // may need to add the background as an actor
-        for (TextButton button : buttons){
+		int ii = 0;
+        for (TextButton button : buttons) {
         	this.addActor(button);
-        } 
-        for(TextButton button : buttons){
+        	this.buttons.add(button);
         	button.setSize(345, 90);
-        }
+        	button.addListener(getListener(ii++));
+        } 
         buttons.get(0).setPosition(1515, 380);
         buttons.get(1).setPosition(90, 380);	
-        if(!TalesOfAndrogyny.patron)
+        	
+        selection = 1;
+        selected(0);
+        if(!TalesOfAndrogyny.patron) {
         	buttons.get(1).setTouchable(Touchable.disabled);
+        	buttons.get(1).setColor(Color.GRAY);
+        	this.buttons.removeIndex(1);
+        }
+	}
+	
+	private ClickListener getListener(final int selection) {
+		return new ClickListener() {
+	        @Override
+	        public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+	        	selected(selection);
+	        }
+	    };
 	}
 
+	private void selected(int selection) {
+		TextButton button = buttons.get(this.selection);
+	 	button.setColor(Color.WHITE);
+	 	button = buttons.get(selection);
+	 	button.setColor(Color.FOREST);		
+	 	this.selection = selection;
+	}
+	
 	@Override
     public void draw(Batch batch, float parentAlpha) {
 		super.draw(batch, parentAlpha);
+		 if(Gdx.input.isKeyJustPressed(Keys.LEFT)) {
+	        	if (selection > 0) selected(selection - 1);
+	        	else selected(buttons.size - 1);
+	        }
+	        else if(Gdx.input.isKeyJustPressed(Keys.RIGHT)) {
+	        	if (selection < buttons.size- 1) selected(selection + 1);
+	        	else selected(0);
+	        }
+	        else if(Gdx.input.isKeyJustPressed(Keys.ENTER)) {
+	        	InputEvent event1 = new InputEvent();
+	            event1.setType(InputEvent.Type.touchDown);
+	            buttons.get(selection).fire(event1);
+
+	            InputEvent event2 = new InputEvent();
+	            event2.setType(InputEvent.Type.touchUp);
+	            buttons.get(selection).fire(event2);
+	        }
     }
 	
-	public int getCode(){
+	public int getCode() {
 		return sceneCode;
 	}
 	
-	public void finish(){
+	public void finish() {
 		isActive = false;
     	addAction(Actions.hide());
 	}
