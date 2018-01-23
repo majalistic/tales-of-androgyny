4119670c93021524386befa519263ce0b36fa9c5
Majalis <majalistic@gmail.com>
2018-01-23 14:11:25 -0500

    Added time display to encounter screen.

--- core/src/com/majalis/screens/EncounterScreen.java
+++ core/src/com/majalis/screens/EncounterScreen.java
@@ -7,14 +7,17 @@ import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Array;
 import com.majalis.asset.AssetEnum;
+import com.majalis.character.PlayerCharacter;
 import com.majalis.encounter.Encounter;
 import com.majalis.encounter.EncounterBuilder.Branch;
 import com.majalis.save.LoadService;
@@ -44,16 +47,20 @@ public class EncounterScreen extends AbstractScreen {
 	private final TextButton saveButton;
 	private final TextButton skipButton;
 	private final TextButton autoplayButton;
+	private final Label dateLabel;
+	private final Skin skin;
 	private boolean skipHeld;
 	
 	protected EncounterScreen(ScreenFactory screenFactory, ScreenElements elements, LoadService loadService, Encounter encounter) {
 		super(screenFactory, elements, null);
 		this.loadService = loadService;
 		this.encounter = encounter;
-		Skin skin = assetManager.get(AssetEnum.UI_SKIN.getSkin());
+		PlayerCharacter character = loadService.loadDataValue(SaveEnum.PLAYER, PlayerCharacter.class);
+		skin = assetManager.get(AssetEnum.UI_SKIN.getSkin());
 		saveButton = new TextButton("Save", skin);
 		skipButton = new TextButton("Skip", skin);
 		autoplayButton = new TextButton("Auto", skin);
+		dateLabel = new DateLabel(character);
 		if (Gdx.app.getPreferences("tales-of-androgyny-preferences").getBoolean("autoplay", false)) autoplayButton.setColor(Color.YELLOW);
 	}
 
@@ -64,6 +71,9 @@ public class EncounterScreen extends AbstractScreen {
 		}
 		
 		final Sound buttonSound = assetManager.get(AssetEnum.BUTTON_SOUND.getSound());
+		
+		dateLabel.setPosition(1650, 325);
+		this.addActor(dateLabel);
 		
 		saveButton.setPosition(1650, 100);
 		saveButton.setWidth(150);
@@ -127,12 +137,14 @@ public class EncounterScreen extends AbstractScreen {
 			saveButton.addAction(Actions.show());
 			skipButton.addAction(Actions.show());
 			autoplayButton.addAction(Actions.show());
+			dateLabel.addAction(Actions.show());
 		}
 		else {
 			skipHeld = false;
 			saveButton.addAction(Actions.hide());
 			skipButton.addAction(Actions.hide());
 			autoplayButton.addAction(Actions.hide());
+			dateLabel.addAction(Actions.hide());
 		}
 		if (encounter.isSwitching()) {
 			showScreen(ScreenEnum.CONTINUE);
@@ -169,4 +181,17 @@ public class EncounterScreen extends AbstractScreen {
 		return requirements;
 	}
 
+	private class DateLabel extends Label {
+		private final PlayerCharacter character;
+		private DateLabel(PlayerCharacter character) {
+			super("Day: " + (character.getTime() / 6 + 1) + "\n" + TimeOfDay.getTime(character.getTime()).getDisplay(), skin);
+			this.character = character;
+		}
+		@Override
+		public void draw(Batch batch, float parentAlpha) {
+			setText("Day: " + (character.getTime() / 6 + 1) + "\n" + TimeOfDay.getTime(character.getTime()).getDisplay());
+			setColor(TimeOfDay.getTime(character.getTime()).getColor());
+			super.draw(batch, parentAlpha);
+		}
+	}
 }
