c25bf6428ce8df718deaae478a54f46b38f6b02c
Majalis <majalistic@gmail.com>
2018-01-25 16:07:26 -0500

    More help screen text.

--- core/src/com/majalis/screens/HelpScreen.java
+++ core/src/com/majalis/screens/HelpScreen.java
@@ -38,10 +38,55 @@ public class HelpScreen extends AbstractScreen{
 		final Sound sound = assetManager.get(AssetEnum.BUTTON_SOUND.getSound());
 
 		Array<Array<Label>> info = new Array<Array<Label>>();
-		info.add(new Array<Label>(new Label[]{new Label("CONTROLS:\n\nPress TAB to toggle hiding or showing the UI.\nPress CTRL to skip text in encounters. Text will be skipped until the next branching choice.\nPress SHIFT to alternate certain images and animations.\nClick and drag or use the arrow keys to navigate the world map.", skin)}));
-		info.add(new Array<Label>(new Label[]{new Label("BASIC GAMEPLAY:\n\nYour stance is the primary determinant in what techniques you can use at a given time. Using a technique will often change your stance. Pay close attention to your stance, the enemy's stance, and what stance a given technique will leave you in if it's successful.\nWhile grappling, your available techniques will be limited based on your position in the grapple - some techniques will only be available if you have the advantage, others if you're at a disadvantage, and this is true of your opponent as well!\nCertain enemies can be defeated by achieving certain conditions in battle, rather than reducing their HP to 0. Similarly, certain enemies can defeat you without reducing your HP to 0!\nBe wary when using skills that cause instability - the enemy can also cause you to overbalance, causing you to trip before you expected to! A technique labelled in red will cause you to fall - one in yellow puts you in dangerous territory the enemy might capitalize on.", skin)}));
-		info.add(new Array<Label>(new Label[]{new Label("BASIC GAMEPLAY (2):\n\nYour character and the enemy are not always at their best - various afflictions temporarily lower their attributes, making them less effective.\nWhen your health is low, you'll receive penalties to the three physical stats - Strength, Endurance, and Agility.\nWhen your stamina is low, you'll receive penalties to Strength and Agility. These penalties apply in encounter checks as well as battle.", skin)}));
-		info.add(new Array<Label>(new Label[]{new Label("COMBAT TECHNIQUES:\n\nAn attack that does 0 damage is not without its use, if it inflicts damage to the opponent's armor/shield or destabilizes them, or if it changes the character's stance to a more advantageous one.\nSome techniques get conditional bonuses - having greater or extra effects if, for example, an enemy is on the ground or on unstable footing, or if you're more agile than the enemy, or simply for being more proficient at the technique.\nSome weapons cause bleed - the amount inflicted depends on the technique. Higher Endurance will reduce damage from bleeding. Bleed can be cured by bandages and rest - Endurance also increases bleed recovery.", skin)}));
+		info.add(new Array<Label>(new Label[]{new Label(
+			"CONTROLS:\n\n" +
+			"Press TAB to toggle hiding or showing the UI.\n\n" +
+			"Press CTRL to skip text in encounters. Text will be skipped until the next branching choice.\n\n" +
+			"Press SHIFT to alternate certain images and animations.\n\n" + 
+			"Click and drag or use the arrow keys to navigate the world map.", 
+			skin
+		)}));
+		info.add(new Array<Label>(new Label[]{new Label(
+			"BASIC GAMEPLAY:\n\n" + 
+			"Your stance is the primary determinant in what techniques you can use at a given time. Using a technique will often change your stance. Pay close attention to your stance, the enemy's stance, and what stance a given technique will leave you in if it's successful.\n\n" + 
+			"While grappling, your available techniques will be limited based on your position in the grapple - some techniques will only be available if you have the advantage, others if you're at a disadvantage, and this is true of your opponent as well!\n\n" + 
+			"Certain enemies can be defeated by achieving certain conditions in battle, rather than reducing their HP to 0. Similarly, certain enemies can defeat you without reducing your HP to 0!\nBe wary when using skills that cause instability - the enemy can also cause you to overbalance, causing you to trip before you expected to! A technique labelled in red will cause you to fall - one in yellow puts you in dangerous territory the enemy might capitalize on.", 
+			skin
+		)}));
+		info.add(new Array<Label>(new Label[]{new Label(
+			"BASIC GAMEPLAY (2):\n\n" + 
+			"Your character and the enemy are not always at their best - various afflictions temporarily lower their attributes, making them less effective.\n\n" +
+			"When your health is low, you'll receive penalties to the three physical stats - Strength, Endurance, and Agility.\n\n" + 
+			"When your stamina is low, you'll receive penalties to Strength and Agility. These penalties apply in encounter checks as well as battle.", 
+			skin
+		)}));
+		info.add(new Array<Label>(new Label[]{new Label(
+			"ATTRIBUTES:\n\n" + 
+			"Strength increases damage and allows you to overpower your enemy in various situations (grappling, for instance).\n\n" +
+			"Endurance increases stamina regen.\n\n" + 
+			"Agility increases stability.", 
+			skin
+		)}));
+		info.add(new Array<Label>(new Label[]{new Label(
+			"COMBAT:\n\n" + 
+			"Damage is INCREASED by\n" +
+			"    Strength (for physical attacks) / Magic (for spells)\n" +
+			"    Weapon Damage (increased by stats)\n" +
+			"    Skill modifier\n\n" +
+			"Damage is DECREASED by\n" +
+			"    Target Base Defense\n" + 
+			"    Target Armor (if armor isn't ignored) - Damage is reduced by armor, which is broken over the course of a battle. Sundering an enemy's armor early in the fight can be a useful investment.\n" + 
+			"    Target Guarding/Parrying (if opponent attack has them)\n" + 
+			"    Skill modifier\n",
+			skin
+		)}));	
+		info.add(new Array<Label>(new Label[]{new Label(
+			"COMBAT TECHNIQUES:\n\n" + 
+			"An attack that does 0 damage is not without its use, if it inflicts damage to the opponent's armor/shield or destabilizes them, or if it changes the character's stance to a more advantageous one.\n\n" + 
+			"Some techniques get conditional bonuses - having greater or extra effects if, for example, an enemy is on the ground or on unstable footing, or if you're more agile than the enemy, or simply for being more proficient at the technique.\n\n" + 
+			"Some weapons cause bleed - the amount inflicted depends on the technique. Higher Endurance will reduce damage from bleeding. Bleed can be cured by bandages and rest - Endurance also increases bleed recovery.", 
+			skin
+		)}));
 		
 		for (int ii = 0; ii < info.size; ii++) {
 			Table newTable = new Table();
