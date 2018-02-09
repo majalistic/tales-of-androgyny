ba5ae14a75d529837aa85569233a28ab1f467581
Majalis <majalistic@gmail.com>
2018-02-08 20:30:43 -0500

    Refactor mutation class.

--- core/src/com/majalis/scenes/Mutation.java
+++ core/src/com/majalis/scenes/Mutation.java
@@ -9,35 +9,22 @@ import com.majalis.save.SaveService;
  * Component of TextScenes that allows them to mutate the gamestate.  Mutations have a save attribute to mutate and know whether to overwrite or read and modify.  Will likely need a MutationBuilder with a fluent interface
  */
 public class Mutation {
-
 	private final SaveService saveService;
-	private SaveEnum path;
-	private ProfileEnum pathProfile;
+	private final SaveEnum path;
+	private final ProfileEnum pathProfile;
 	private final Object value;
-		
-	public Mutation(SaveService saveService, SaveEnum path, Object value) {
+	public Mutation(SaveService saveService, SaveEnum path, Object value) { this(saveService, path, null, value); }	
+	public Mutation(SaveService saveService, ProfileEnum pathProfile, Object value) { this(saveService, null, pathProfile, value); }	
+	private Mutation(SaveService saveService, SaveEnum path, ProfileEnum pathProfile, Object value) {
 		this.saveService = saveService;
 		this.path = path;
-		this.value = value;
-	}
-	
-	public Mutation(SaveService saveService, ProfileEnum pathProfile, Object value) {
-		this.saveService = saveService;
 		this.pathProfile = pathProfile;
 		this.value = value;
 	}
 
-	public  Array<MutationResult> mutate() {
-		if (saveService == null) {
-			return null;
-		}
-		if (path != null) {
-			return saveService.saveDataValue(path, value, false);
-		}
-		else {
-			saveService.saveDataValue(pathProfile, value, true);
-			return null;
-		}
+	public Array<MutationResult> mutate() {
+		if (saveService == null) { return null; }
+		if (path != null) { return saveService.saveDataValue(path, value, false); }
+		else { saveService.saveDataValue(pathProfile, value, true); return null; }
 	}
-	
 }
