20c3d90cad240561873471915aee48b5ed41ad2e
Majalis <majalistic@gmail.com>
2018-03-01 18:01:52 -0500

    Prevented gaps on world map.

--- core/src/com/majalis/world/Zone.java
+++ core/src/com/majalis/world/Zone.java
@@ -68,16 +68,13 @@ public class Zone {
 		int minimumXY = 200;
 		int maxX = 130;
 		int maximumXY = 380;
-		
+		int nodeCode;
 		for (int ii = 0; ii < repeats; ii++) {
 			for (GameWorldNode requiredNode : requiredNodes) {
 				boolean nodeNotReached = true;
 				GameWorldNode currentNode = startNode;
 				GameWorldNode closestNode = currentNode;
-				if (requiredNodesUnfulfilled.contains(requiredNode, true)) {
-					closestNode = getClosestNode(requiredNode, nodes);
-				}
-				for (int nodeCode = nodes.size; nodeNotReached; ) {
+				for (nodeCode = nodes.size; nodeNotReached; ) {
 					Vector2 newNodePosition = null; // this should never happen
 					Vector2 source = new Vector2(currentNode.getHexPosition());
 					// valid walk positions:
@@ -159,6 +156,97 @@ public class Zone {
 			}
 		}
 		
+		for (GameWorldNode unfulfilledNode : requiredNodesUnfulfilled) {
+			GameWorldNode closest = null;
+			for (GameWorldNode node : nodes) {
+				if (node == unfulfilledNode) continue;
+				int distanceToNode = unfulfilledNode.getDistance(node);
+				if (closest == null || closest.getDistance(unfulfilledNode) > distanceToNode) { closest = node; }
+			}
+			if (closest != null) {
+				//unfulfilledNode.connectTo(closest);	
+				boolean nodeNotReached = true;
+				GameWorldNode currentNode = closest;
+				GameWorldNode closestNode = currentNode;
+				for (nodeCode = nodes.size; nodeNotReached; ) {
+					Vector2 newNodePosition = null; // this should never happen
+					Vector2 source = new Vector2(currentNode.getHexPosition());
+					// valid walk positions:
+					// 1. position must be > 5 hexes away from source node - therefore, EITHER x, y, or z deltas must be > 5
+					// 2. position must be > 5 hexes away from target node
+					// 3. position must be <= 11 hexes away from source node
+					// 4. position should not be a greater distance from the target node - if all nodes that take us closer are taken, take us further
+					// 5. source and target are greater than 5 distance away, so the only two possible cases are "source and target are between 6 and 11 tiles away from each other" and source and target are 12+ tiles away from each other
+					
+					int currentDistance = currentNode.getDistance(unfulfilledNode);
+					// create a set of possible coordinates
+					Array<Vector2> possibleTowardsCoordinates = new Array<Vector2>();
+					Array<Vector2> possibleAwayCoordinates = new Array<Vector2>();
+					Vector2 possible = new Vector2(0, 0);
+					for (int jj = Math.max((int)source.x - 11, minimumX); jj < Math.min((int)source.x + 11, maxX); jj++) {
+						for (int kk = (int)source.y - 11; kk < source.y + 11; kk++) {
+							int xy = jj + kk * 2;
+							if (xy < minimumXY || xy >= maximumXY) continue;
+							possible.x = jj;
+							possible.y = kk;
+							int newDistance = unfulfilledNode.getDistance(possible);
+							if (!currentNode.isOverlapping(possible) && currentNode.isAdjacent(possible) && newDistance >= 6) { 
+								if (newDistance <= currentDistance) { // only accepts a candidate position if it doesn't take us further away from the target - need to create a list of those that do and use them as backup
+									possibleTowardsCoordinates.add(new Vector2(possible));
+								}
+								else {
+									possibleAwayCoordinates.add(new Vector2(possible));
+								}
+							}
+						}
+					}
+					
+					boolean overlap = true;
+					while (overlap && possibleTowardsCoordinates.size > 0) {
+						// choose a random one
+						newNodePosition = possibleTowardsCoordinates.get(Math.abs(random.nextInt() % possibleTowardsCoordinates.size));
+						// check to see if it overlaps, remove it from the set of possible coordinates if it is invalid
+						overlap = isOverlap(newNodePosition, nodes);
+						if (overlap) {
+							possibleTowardsCoordinates.removeValue(newNodePosition, true);
+						}
+					}
+					if (possibleTowardsCoordinates.size == 0) {
+						overlap = true;
+						while (overlap && possibleAwayCoordinates.size > 0) {
+							// choose a random one
+							newNodePosition = possibleAwayCoordinates.get(Math.abs(random.nextInt() % possibleAwayCoordinates.size));
+							// check to see if it overlaps, remove it from the set of possible coordinates if it is invalid
+							overlap = isOverlap(newNodePosition, nodes);
+							if (overlap) {
+								possibleAwayCoordinates.removeValue(newNodePosition, true);
+							}
+						}
+						if (possibleAwayCoordinates.size == 0) {
+							if (currentNode == closestNode) {
+								break;
+							}
+							currentNode = closestNode;
+							continue;
+						}
+					}
+					
+					GameWorldNode newNode = getNode(
+						nodeCode, 
+						TalesOfAndrogyny.setEncounter.size == 0 ? EncounterCode.getEncounterCode(nodeCode - 1, difficulty, unspawnedEncounters) : TalesOfAndrogyny.setEncounter.get(nodeCode % TalesOfAndrogyny.setEncounter.size),
+						EncounterCode.DEFAULT, TalesOfAndrogyny.randomEncounters ? EncounterCode.getDifficultySet(difficulty) : new Array<EncounterCode>(), (int)newNodePosition.x, (int)newNodePosition.y, visitedInfo.get(nodeCode, getFreshVisitInfo(true)));
+					addNode(newNode, nodeCode, nodes);
+					
+					// if we've reached the target node, we can terminate this run-through
+					nodeNotReached = !unfulfilledNode.isAdjacent(newNode);
+					// save the node for the next iteration
+					currentNode = newNode;		
+					nodeCode++;
+					if (closestNode.getDistance(unfulfilledNode) > newNode.getDistance(unfulfilledNode)) closestNode = newNode;
+				}
+			}
+		}
+		
 		// connect all nodes that consider themselves adjacent to nearby nodes - some nodes, like permanent nodes, might have a longer "reach" then others
 		for (int ii = 0; ii < nodes.size-1; ii++) {
 			for (int jj = ii + 1; jj < nodes.size; jj++) {
@@ -180,25 +268,6 @@ public class Zone {
 			}
 		}
 		return this;
-	}
-	
-	private GameWorldNode getClosestNode(GameWorldNode targetNode, Array<GameWorldNode> nodes) {
-		GameWorldNode closestNode = nodes.get(0);
-		int shortestDistance = closestNode.getDistance(targetNode);
-		if (closestNode == targetNode) {
-			closestNode = nodes.get(1);
-			shortestDistance = closestNode.getDistance(targetNode);
-		}
-		
-		for (GameWorldNode node : nodes) {
-			if (node == targetNode) continue;
-			int distance = node.getDistance(targetNode);
-			if (distance < shortestDistance) {
-				shortestDistance = distance;
-				closestNode = node;
-			}
-		}
-		return closestNode;
 	}
 	
 	private boolean isOverlap(Vector2 newNodePosition, Array<GameWorldNode> nodes) {
