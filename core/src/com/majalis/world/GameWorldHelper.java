272d834685202612af442fd31e96771dab13cccc
Majalis <majalistic@gmail.com>
2018-03-01 20:53:53 -0500

    Gameworldhelper refactor.

--- core/src/com/majalis/world/GameWorldHelper.java
+++ core/src/com/majalis/world/GameWorldHelper.java
@@ -11,19 +11,8 @@ public class GameWorldHelper {
 	public static int getTileWidth() { return tileWidth; }
 	public static int getTileHeight() { return tileHeight; }
 	
-	public static int getTrueX(int x) {
-		return (x - 16) * (scalingFactor + xFactor);
-	}
-	
-	public static int getTrueY(int x, int y) {
-		return (y - 85) * scalingFactor + (x - 16) * scalingFactor / 2;
-	}
-	
-	public static Vector2 calculatePosition(int x, int y) {
-		return new Vector2(getTrueX(x), getTrueY(x, y));
-	}
-	
-	public static int distance(int x, int y, int x2, int y2) {
-		return Math.max(Math.max(Math.abs(x - x2), Math.abs(y - y2)), Math.abs((0 - (x + y)) - (0 - (x2 + y2))));				
-	}
+	public static int getTrueX(int x) { return (x - 16) * (scalingFactor + xFactor); }
+	public static int getTrueY(int x, int y) { return (y - 85) * scalingFactor + (x - 16) * scalingFactor / 2; }
+	public static Vector2 calculatePosition(int x, int y) { return new Vector2(getTrueX(x), getTrueY(x, y)); }
+	public static int distance(int x, int y, int x2, int y2) { return Math.max(Math.max(Math.abs(x - x2), Math.abs(y - y2)), Math.abs((0 - (x + y)) - (0 - (x2 + y2)))); }
 }
