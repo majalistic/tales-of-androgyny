768111f672e1518ef8a304b477f64ef6a9f1c885
Majalis <majalistic@gmail.com>
2018-03-03 18:29:41 -0500

    Warlock anal route.

--- core/src/com/majalis/encounter/EncounterCode.java
+++ core/src/com/majalis/encounter/EncounterCode.java
@@ -1744,7 +1744,12 @@ public enum EncounterCode {
 						b.branch(true).textScene("WARLOCK-ENTER").battleScene(
 							BattleCode.WARLOCK, 
 							b.branch(Outcome.VICTORY).textScene("WARLOCK-VICTORY"), 
-							b.branch(Outcome.DEFEAT).textScene("WARLOCK-DEFEAT").choiceScene("What do you choose?", b.branch("To love ass-sex").textScene("WARLOCK-ANAL-ADDICT"), b.branch("To love dicks").textScene("WARLOCK-COCK-LOVER"), b.branch("To be a girl").textScene("WARLOCK-FEMINIZATION"))
+							b.branch(Outcome.DEFEAT).textScene("WARLOCK-DEFEAT").choiceScene(
+								"What do you choose?", 
+								b.branch("To love ass-sex").checkScene(Perk.ANAL_ADDICT, b.branch(3).textScene("WARLOCK-ANAL-ADDICT").textScene("WARLOCK-ANAL-BROKEN").gameEnd(), b.branch(0).textScene("WARLOCK-ANAL-ADDICT").textScene("WARLOCK-ANAL-UNBROKEN")), 
+								b.branch("To love dicks").textScene("WARLOCK-COCK-LOVER"), 
+								b.branch("To be a girl").textScene("WARLOCK-FEMINIZATION")
+							)
 						), 
 						b.branch(false).textScene("WARLOCK-REVISIT")
 					)
