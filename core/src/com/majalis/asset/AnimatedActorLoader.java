2b20dfadad73cc24ce25ac9c147fe08e501211b5
Majalis <majalistic@gmail.com>
2018-03-07 15:59:52 -0500

    Cached animations to mitigate lack of TextureAtlas disposal.

--- core/src/com/majalis/asset/AnimatedActorLoader.java
+++ core/src/com/majalis/asset/AnimatedActorLoader.java
@@ -8,23 +8,25 @@ import com.badlogic.gdx.assets.loaders.SynchronousAssetLoader;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ObjectMap;
 import com.esotericsoftware.spine.SkeletonData;
 import com.esotericsoftware.spine.SkeletonJson;
 import com.esotericsoftware.spine.SkeletonMeshRenderer;
 
 public class AnimatedActorLoader extends SynchronousAssetLoader<AnimatedActorFactory, AnimatedActorLoader.AnimatedActorParameter> {
 
+	private final ObjectMap<String, TextureAtlas> cachedAtlas;
+	
 	public AnimatedActorLoader(FileHandleResolver resolver) {
 		super(resolver);
+		this.cachedAtlas = new ObjectMap<String, TextureAtlas>();
 	}
 	
 	@Override
-	public AnimatedActorFactory load(AssetManager assetManager, String fileName, FileHandle file, AnimatedActorParameter parameter) {
-		return getActor(fileName, file, parameter);
-	}
-	
+	public AnimatedActorFactory load(AssetManager assetManager, String fileName, FileHandle file, AnimatedActorParameter parameter) { return getActor(fileName, file, parameter); }
 	private AnimatedActorFactory getActor(String fileName, FileHandle file, AnimatedActorParameter parameter) {
-		TextureAtlas atlas = new TextureAtlas(file);
+		TextureAtlas atlas = cachedAtlas.containsKey(file.path()) ? cachedAtlas.get(file.path()) : new TextureAtlas(file);
+		cachedAtlas.put(file.path(), atlas);
 		SkeletonMeshRenderer renderer = new SkeletonMeshRenderer();
 		renderer.setPremultipliedAlpha(true);
 		
@@ -60,6 +62,4 @@ public class AnimatedActorLoader extends SynchronousAssetLoader<AnimatedActorFac
 		}
 		
 	}
-
-
 }
