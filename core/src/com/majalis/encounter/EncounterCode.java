c37e8a5568154612704a0c455a9b601052799053
Majalis <majalistic@gmail.com>
2018-04-04 18:41:51 -0400

    Giantess speak attempt.

--- core/src/com/majalis/encounter/EncounterCode.java
+++ core/src/com/majalis/encounter/EncounterCode.java
@@ -1111,6 +1111,7 @@ public enum EncounterCode {
 							b.branch(0).textScene("GIANTESS-UNAWARE").choiceScene(
 								"What do you do?", 
 								b.branch("Stand and fight").textScene("GIANTESS-FIGHT").battleScene(BattleCode.GIANTESS, 1, b.branch(Outcome.VICTORY), b.branch(Outcome.DEFEAT).concat(rebirth)),
+								b.branch("Speak").textScene("GIANTESS-SPEAK").concat(rebirth),
 								b.branch("Run and hide").textScene("GIANTESS-HIDE"), 
 								b.branch("Climb her").textScene("GIANTESS-CLIMB").checkScene(
 									Stat.AGILITY, 
