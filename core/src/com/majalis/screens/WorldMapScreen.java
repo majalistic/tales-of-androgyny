3fb1f1b4ee45850c0dd504a5baddde4bc23f9d68
Majalis <majalistic@gmail.com>
2018-05-04 10:08:02 -0400

    Directly manipulate camera's z position.

--- core/src/com/majalis/screens/WorldMapScreen.java
+++ core/src/com/majalis/screens/WorldMapScreen.java
@@ -1136,8 +1136,6 @@ public class WorldMapScreen extends AbstractScreen {
 	private Color getTimeColor() { return TimeOfDay.getTime(time).getColor(); }
 	private String getTime() { return TimeOfDay.getTime(time).getDisplay(); }
 	
-	private static int distance = 750;
-	
 	private void translateCamera(float delta) {
 		Vector3 translationVector = new Vector3(0, 0, 0);
 		float speed = 500 * delta;
@@ -1157,14 +1155,13 @@ public class WorldMapScreen extends AbstractScreen {
 		}
 		
 		if (Gdx.input.isKeyPressed(Keys.A)) {
-			distance += 5;
+			camera.position.z += 5;
 		}
 		if (Gdx.input.isKeyPressed(Keys.S)) {
-			distance -= 5;
+			camera.position.z -= 5;
 		}
 		
-		distance = Math.max(Math.min(distance, 1500), 500);
-		camera.position.z = distance;
+		camera.position.z = Math.max(Math.min(camera.position.z, 1500), 500);
 		translateCamera(translationVector);
 	}
 	
