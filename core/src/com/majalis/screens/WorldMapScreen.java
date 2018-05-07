9d63fb6c3fadb762b1e1c4f54b709aed41ee924c
Majalis <majalistic@gmail.com>
2018-05-07 18:24:28 -0400

    Implemented pinch zooming on world map.

--- core/src/com/majalis/screens/WorldMapScreen.java
+++ core/src/com/majalis/screens/WorldMapScreen.java
@@ -166,7 +166,7 @@ public class WorldMapScreen extends AbstractScreen {
 		
 		gestureDetection = new GestureDetector(new GestureDetector.GestureListener() {		
 			@Override
-			public boolean zoom(float initialDistance, float distance) { return false; }
+			public boolean zoom(float initialDistance, float distance) { translateCamera(new Vector3(0, 0, initialDistance - distance)); return true; }
 			
 			@Override
 			public boolean touchDown(float x, float y, int pointer, int button) { return false; }
