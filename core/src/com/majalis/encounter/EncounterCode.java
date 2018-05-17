d9ba15f38c73dc554d6e077a629c915f8751e795
Majalis <majalistic@gmail.com>
2018-05-17 16:15:38 -0400

    Beastmistress pure ending.

--- core/src/com/majalis/encounter/EncounterCode.java
+++ core/src/com/majalis/encounter/EncounterCode.java
@@ -461,7 +461,7 @@ public enum EncounterCode {
 			case BEASTMISTRESS:
 				Branch mistressChoice = b.branch("Meow").textScene("BEASTMISTRESS-ACCEPT");		
 				Branch kittyImpure = b.branch(1).textScene("BEASTMISTRESS-IMPURE").checkScene(Perk.BEASTMASTER, b.branch(3).checkScene(CheckType.ANY_WILLPOWER, b.branch(true).textScene("BEASTMISTRESS-RESIST2"), b.branch(false).textScene("BEASTMISTRESS-QUEEN").gameEnd()), b.branch(0).textScene("BEASTMISTRESS-IMPURE-END"));
-				Branch kittySex = b.branch().checkScene(Perk.BEASTMASTER, kittyImpure, b.branch(0).textScene("BEASTMISTRESS-PURE"));
+				Branch kittySex = b.branch().textScene("BEASTMISTRESS-KITTY2").checkScene(Perk.BEASTMASTER, kittyImpure, b.branch(0).textScene("BEASTMISTRESS-PURE"));
 				Branch kittyForeplay = b.branch().textScene("BEASTMISTRESS-KITTY").checkScene(CheckType.FREE_COCK, b.branch(true).textScene("BEASTMISTRESS-NONCAGED").concat(kittySex), b.branch(false).textScene("BEASTMISTRESS-CAGED").concat(kittySex));
 				
 				return b.branch().textScene("BEASTMISTRESS-INTRO").choiceScene(
